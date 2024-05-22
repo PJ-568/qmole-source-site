5a3f7797c4b5e4f27e8c018094aed45251bd8f9b
PJ568 <PJ568@PJ568.eu.org>
2024-05-22 08:40:56 +0800

    【优化】数据结构

--- js/load_ani.js
+++ js/load_ani.js
@@ -1,5 +1,5 @@
-let loadingBar = document.querySelector(".loading-bar");
-let progress = document.querySelector(".loading-bar .progress");
+const loadingBar = document.querySelector(".loading-bar");
+const progress = document.querySelector(".loading-bar .progress");
 
 var timer = null;
 
